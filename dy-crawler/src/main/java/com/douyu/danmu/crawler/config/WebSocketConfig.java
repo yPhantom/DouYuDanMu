c3ae9adcb6f3352bee1bac7ba2f23dded862a8f9
gyx2019 <541004828@qq.com>
2021-03-10 23:58:16 +0800

    git init

---
+++ dy-crawler/src/main/java/com/douyu/danmu/crawler/config/WebSocketConfig.java
@@ -0,0 +1,48 @@
+package com.douyu.danmu.crawler.config;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ServerHandshake;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+
+@Slf
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public WebSocketClient webSocketClient() {
+        try {
+            WebSocketClient webSocketClient = new WebSocketClient(new URI("ws://localhost:8085/websocket/test"),new Draft_6455()) {
+                @Override
+                public void onOpen(ServerHandshake handshakedata) {
+                    log.info("[websocket] 连接成功");
+                }
+
+                @Override
+                public void onMessage(String message) {
+                    log.info("[websocket] 收到消息={}",message);
+
+                }
+
+                @Override
+                public void onClose(int code, String reason, boolean remote) {
+                    log.info("[websocket] 退出连接");
+                }
+
+                @Override
+                public void onError(Exception ex) {
+                    log.info("[websocket] 连接错误={}",ex.getMessage());
+                }
+            };
+            webSocketClient.connect();
+            return webSocketClient;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
